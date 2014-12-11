ccb1209643df05ad21b680e4693f6991ccbc6161
Joe Stein <joeaarons@gmail.com>
2014-12-11 17:03:29 -0500

    License and notes on the class

--- src/main/java/me/josephstein/oss/randomandroid/DeactivatableLinkMovementMethod.java
+++ src/main/java/me/josephstein/oss/randomandroid/DeactivatableLinkMovementMethod.java
@@ -9,8 +9,30 @@ import android.text.style.ClickableSpan;
 import android.view.MotionEvent;
 import android.widget.TextView;
 
-/**
- * Sup
+/*
+* Copyright (C) 2006 The Android Open Source Project
+* Copyright (C) 2014 Joseph Stein
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+/*
+ * Class derived from the LinkMovementMethod.
+ * Differs in that it doesn't follow the link if the text widget including the link isn't
+ * pressed when the mouse is released (mouseup).
+ *
+ * That is, if the user presses the link but drags away far enough before releasing, don't follow
+ * the link.
  */
 public class DeactivatableLinkMovementMethod extends LinkMovementMethod {
     @Override
